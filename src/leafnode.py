b4fbb9c2ec032271952b3c2eae2caddd78d4679b
Rob Gilliam <rob_gilliam@hotmail.com>
2024-09-20 23:05:21 +0100

    Add repr methods to subclasses

--- src/leafnode.py
+++ src/leafnode.py
@@ -4,8 +4,15 @@ class LeafNode(HTMLNode):
     def __init__(self, tag, value, props = None):
         super().__init__(tag, value, None, props)
 
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+
     def to_html(self):
         if self.value is None:
             raise ValueError("LeafNode requires a value")
+        if self.tag is None:
+            return self.Value
 
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/parentnode.py
+++ src/parentnode.py
@@ -4,6 +4,9 @@ class ParentNode(HTMLNode):
     def __init__(self, tag, children, props = None):
         super().__init__(tag, None, children, props)
 
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
+
     def to_html(self):
         if self.tag is None:
             raise ValueError("ParentNode requires a tag")
