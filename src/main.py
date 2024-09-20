45916a4c6211c3e2690ae5231aa50f81e504905b
Rob Gilliam <rob_gilliam@hotmail.com>
2024-09-20 21:27:38 +0100

    Add LeafNode and tests

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from textnode import *
 from htmlnode import *
+from leafnode import *
 
 def main():
     text_node = TextNode("This is a text node", "bold", "https://www.boot.dev")
@@ -11,4 +12,12 @@ def main():
     print(html_node)
     print(html_node.props_to_html())
 
+    paragraph = LeafNode("p", "This is a paragraph of text.")
+    anchor = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+
+    print(paragraph)
+    print(anchor)
+
+    print(paragraph.to_html())
+    print(anchor.to_html())
 main()
---
+++ src/test_leafnode.py
@@ -0,0 +1,48 @@
+import unittest
+
+from leafnode import LeafNode
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_eq(self):
+        node = LeafNode("tag", "Some value")
+        node2 = LeafNode("tag", "Some value")
+        self.assertEqual(node, node2)
+
+    def test_not_eq_tag(self):
+        node = LeafNode("tag", "Some value")
+        node2 = LeafNode("tug", "Some value")
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_value(self):
+        node = LeafNode("tag", "Some value")
+        node2 = LeafNode("tag", "Some other value")
+        self.assertNotEqual(node, node2)
+
+    def test_eq_props(self):
+        node = LeafNode("tag", "Some value", {"fiirst": "one", "second": "two"})
+        node2 = LeafNode("tag", "Some value", {"fiirst": "one", "second": "two"})
+        self.assertEqual(node, node2)
+
+    def test_not_eq_props(self):
+        node = LeafNode("tag", "Some value", {"fiirst": "one", "second": "two"})
+        node2 = LeafNode("tag", "Some value", {"fiirst": "one", "second": "2"})
+        self.assertNotEqual(node, node2)
+
+    def test_props_order_doesnt_matter(self):
+        node = LeafNode("tag", "Some value", {"fiirst": "one", "second": "two"})
+        node2 = LeafNode("tag", "Some value", {"second": "two", "fiirst": "one"})
+        self.assertEqual(node, node2)
+
+    def test_to_html(self):
+        node = LeafNode("tag", "Some value")
+        html = node.to_html()
+        self.assertEqual(html, "<tag>Some value</tag>")
+
+    def test_to_html_with_props(self):
+        node = LeafNode("tag", "Some value", {"first" : "one", "second": "two"})
+        html = node.to_html()
+        self.assertEqual(html, "<tag first=\"one\" second=\"two\">Some value</tag>")
+
+if __name__ == "__main__":
+    unittest.main()
