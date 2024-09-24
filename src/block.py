67ebc957c2b2112d5e8080c9953eeab34445d64f
Rob Gilliam <rob_gilliam@hotmail.com>
2024-09-24 23:13:51 +0100

    Add block type detector

--- src/block.py
+++ src/block.py
@@ -1,3 +1,15 @@
+import re
+
+from functools import reduce
+
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered"
+block_type_ordered_list = "ordered"
+
+
 def markdown_to_blocks(markdown):
     blocks = []
 
@@ -7,3 +19,28 @@ def markdown_to_blocks(markdown):
             blocks.append(block)
 
     return blocks
+
+def block_to_blocktype(block):
+    if re.match(r"^#{1,6} ", block) is not None:
+        return block_type_heading
+
+    if re.match(r"```[^`]", block[:4]) and re.match(r"[^`]```", block [-4:]):
+        return block_type_code
+
+    lines = block.split("\n")
+
+    if all(map(lambda b: b[0] == ">", lines)):
+        return block_type_quote
+
+    if all(map(lambda b: b[:2] == "* ", lines)):
+        return block_type_unordered_list
+
+    if all(map(lambda b: b[:2] == "- ", lines)):
+        return block_type_unordered_list
+
+    for i in range(len(lines)):
+        matches = re.findall(r"^(\d+)\. ", lines[i])
+        if len(matches) != 1 or str(i+1) != matches[0]:
+            return block_type_paragraph
+
+    return block_type_ordered_list
--- src/test_block.py
+++ src/test_block.py
@@ -94,3 +94,157 @@ class TestBlock(unittest.TestCase):
         expected_blocks = ["This is the first block\nThis is still the first block"]
 
         self.assertListEqual(blocks, expected_blocks)
+
+    def test_block_type_paragraph(self):
+        block = "This is just a paragraph of text"
+        block_type = block_to_blocktype(block)
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_block_type_heading(self):
+        for h_num in range(1, 11):
+            with self.subTest(f"Check heading level {h_num}", h_num = h_num):
+                block = "#" * h_num + " heading level " + str(h_num)
+                block_type = block_to_blocktype(block)
+
+                if h_num <= 6:
+                    self.assertEqual(block_type, block_type_heading)
+                else:
+                    self.assertEqual(block_type, block_type_paragraph)
+
+    def test_block_type_code(self):
+        block = "```\nprint(\"Hello, world!\")\nexit(0)```"
+        block_type = block_to_blocktype(block)
+
+        self.assertEqual(block_type, block_type_code)
+
+    def test_block_type_bad_code_too_many_start_backticks(self):
+        block = "````\nprint(\"Hello, world!\")\nexit(0)```"
+        block_type = block_to_blocktype(block)
+
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_block_type_bad_code_too_many_end_backticks(self):
+        block = "```\nprint(\"Hello, world!\")\nexit(0)````"
+        block_type = block_to_blocktype(block)
+
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_block_type_bad_code_too_few_start_backticks(self):
+        block = "``\nprint(\"Hello, world!\")\nexit(0)```"
+        block_type = block_to_blocktype(block)
+
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_block_type_bad_code_too_few_end_backticks(self):
+        block = "```\nprint(\"Hello, world!\")\nexit(0)``"
+        block_type = block_to_blocktype(block)
+
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_block_type_bad_code_missing_start_backticks(self):
+        block = "print(\"Hello, world!\")\nexit(0)```"
+        block_type = block_to_blocktype(block)
+
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_block_type_bad_code_missing_end_backticks(self):
+        block = "````\nprint(\"Hello, world!\")\nexit(0)"
+        block_type = block_to_blocktype(block)
+
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_block_type_quote(self):
+        block = "> I wondered lonely as a cloud\n> That floats on high o'er dales and hills\n> When all at once I saw a crowd\n> That confiscated all my pills"
+        block_type  = block_to_blocktype(block)
+
+        self.assertEqual(block_type, block_type_quote)
+
+    def test_block_type_bad_quote_missing_gt_at_start(self):
+        block = "I wondered lonely as a cloud\n> That floats on high o'er dales and hills\n> When all at once I saw a crowd\n> That confiscated all my pills"
+        block_type  = block_to_blocktype(block)
+
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_block_type_bad_quote_missing_gt(self):
+        block = "> I wondered lonely as a cloud\n> That floats on high o'er dales and hills\nWhen all at once I saw a crowd\n> That confiscated all my pills"
+        block_type  = block_to_blocktype(block)
+
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_block_type_unordered_list_asterisk(self):
+        block = "* Apples\n* Pears\n* Oranges\n* Lemons"
+        block_type  = block_to_blocktype(block)
+
+        self.assertEqual(block_type, block_type_unordered_list)
+
+    def test_block_type_unordered_list_hyphen(self):
+        block = "- Apples\n- Pears\n- Oranges\n- Lemons"
+        block_type  = block_to_blocktype(block)
+
+        self.assertEqual(block_type, block_type_unordered_list)
+
+    def test_block_type_bad_unordered_list_mixed(self):
+        block = "* Apples\n* Pears\n- Oranges\n- Lemons"
+        block_type  = block_to_blocktype(block)
+
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_block_type_bad_unordered_list_missing_asterisk(self):
+        block = "* Apples\n* Pears\n Oranges\n* Lemons"
+        block_type  = block_to_blocktype(block)
+
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_block_type_bad_unordered_list_missing_hyphen(self):
+        block = "- Apples\n Pears\n- Oranges\n- Lemons"
+        block_type  = block_to_blocktype(block)
+
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_block_type_bad_unordered_list_missing_space_after_asterisk(self):
+        block = "*Apples\n*Pears\n*Oranges\n*Lemons"
+        block_type  = block_to_blocktype(block)
+
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_block_type_bad_unordered_list_missing_space_after_hyphen(self):
+        block = "-Apples\n-Pears\n-Oranges\n-Lemons"
+        block_type  = block_to_blocktype(block)
+
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_block_ordered_list(self):
+        block = "1. Get bread from pack\n2. Put bread in toaster\n3. Wait for toaster to pop\n4. Spread butter on bread\n5. Enjoy!"
+        block_type  = block_to_blocktype(block)
+
+        self.assertEqual(block_type, block_type_ordered_list)
+
+    def test_block_bad_ordered_list_jumbled_sequence(self):
+        block = "1. Get bread from pack\n3. Put bread in toaster\n2. Wait for toaster to pop\n4. Spread butter on bread\n5. Enjoy!"
+        block_type  = block_to_blocktype(block)
+
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_block_bad_ordered_list_skipped_sequence(self):
+        block = "1. Get bread from pack\n3. Put bread in toaster\n4. Wait for toaster to pop\n5. Spread butter on bread\n6. Enjoy!"
+        block_type  = block_to_blocktype(block)
+
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_block_bad_ordered_list_duplicate_number(self):
+        block = "1. Get bread from pack\n2. Put bread in toaster\n2. Wait for toaster to pop\n3. Spread butter on bread\n4. Enjoy!"
+        block_type  = block_to_blocktype(block)
+
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_block_bad_ordered_list_missing_space(self):
+        block = "1. Get bread from pack\n2. Put bread in toaster\n3. Wait for toaster to pop\n4. Spread butter on bread\n5.Enjoy!"
+        block_type  = block_to_blocktype(block)
+
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_block_bad_ordered_list_missing_number(self):
+        block = "1. Get bread from pack\n2. Put bread in toaster\n3. Wait for toaster to pop\n4. Spread butter on bread\nEnjoy!"
+        block_type  = block_to_blocktype(block)
+
+        self.assertEqual(block_type, block_type_paragraph)
